83131e4a223edf801edbb5a4ddc4ff2d97a5b69e
nicolas vonner <nicolas.vonner@icloud.com>
2022-06-03 12:59:17 +0200

    Providers, dossiers settings, implementation sur le main

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,95 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:sakartone/layout/layout.dart';
+import 'package:provider/provider.dart';
+import 'package:sakartone/model/employees_model.dart';
+import 'package:sakartone/providers/employees_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Sakartone());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Sakartone extends StatelessWidget {
+  const Sakartone({Key? key}) : super(key: key);
 
+  static List<Employees>? employees;
+
+// List<Employees> =
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sakartone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        tabBarTheme: const TabBarTheme(
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey,
-            // labelStyle: TextStyle(fontFamily: 'Mantinia'), // color for text
-            indicator: UnderlineTabIndicator(
-              // color for indicator (underline)
-                borderSide:
-                BorderSide(color: Color.fromRGBO(160, 141, 106, 1.0)))),
+    Provider.of<EmployeesProvider>(context, listen: false).fetchData();
+    Sakartone.employees =
+        Provider.of<EmployeesProvider>(context, listen: false).employees;
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<EmployeesProvider>(
+            create: (context) => EmployeesProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Sakartone',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          tabBarTheme: const TabBarTheme(
+              labelColor: Colors.white,
+              unselectedLabelColor: Colors.grey,
+              // labelStyle: TextStyle(fontFamily: 'Mantinia'), // color for text
+              indicator: UnderlineTabIndicator(
+                  // color for indicator (underline)
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(160, 141, 106, 1.0)))),
+        ),
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case Home.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return Home();
+                });
+              }
+            case Stat.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return Stat();
+                });
+              }
+            case EmployeeList.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return EmployeeList();
+                });
+              }
+            case EmployeeDetails.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return EmployeeDetails();
+                });
+              }
+            case EmployeeUpdate.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return EmployeeUpdate();
+                });
+              }
+            case EmployeeCreate.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return EmployeeCreate();
+                });
+              }
+            case TeamList.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return TeamList();
+                });
+              }
+            case TeamDetails.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return TeamDetails();
+                });
+              }
+            case TeamUpdate.routeName:
+              {
+                return MaterialPageRoute(builder: (context) {
+                  return TeamUpdate();
+                });
+              }
+          }
+        },
+        initialRoute: Home.routeName,
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(builder: (context) => const NotFound());
+        },
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
       ),
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case Home.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return Home();
-              });
-            }
-          case Stat.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return Stat();
-              });
-            }
-          case EmployeeList.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return EmployeeList();
-              });
-            }
-          case EmployeeDetails.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return EmployeeDetails();
-              });
-            }
-          case EmployeeUpdate.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return EmployeeUpdate();
-              });
-            }
-          case EmployeeCreate.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return EmployeeCreate();
-              });
-            }
-          case TeamList.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return TeamList();
-              });
-            }
-          case TeamDetails.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return TeamDetails();
-              });
-            }
-          case TeamUpdate.routeName:
-            {
-              return MaterialPageRoute(builder: (context) {
-                return TeamUpdate();
-              });
-            }
-        }
-      },
-      initialRoute: Home.routeName,
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (context) => const NotFound());
-      },
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
